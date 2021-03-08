6d022c8b66078289c6eb4a71d5261ec82dd5ca32
Aphilosopher30 <Ashafer1@fordham.edu>
2021-03-08 16:47:25 -0700

    create appropriate runner files

---
+++ lib/night_writer.rb
@@ -0,0 +1,21 @@
+require './lib/braille_text'
+require './lib/alpha_text'
+require './lib/read_write'
+
+input = ARGV[0]
+output = ARGV[1]
+
+raw_text = ReadWrite.new(input)
+
+if raw_text.is_brail?
+  text = BrailleText.new(raw_text.file)
+else
+  text = AlphaText.new(raw_text.file)
+end
+
+translation = text.printable_translation
+
+translated_doc = File.new("./texts/alpha/#{output}", "w")
+translated_doc.write(translation)
+
+puts "Created '#{output}' containing #{text.character_list.length} characters."
